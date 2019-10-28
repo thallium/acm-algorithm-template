b494aafc4396309dade0315d6a896c2de9cc0600
thallium <tuogengchen@gmail.com>
2019-10-28 11:50:32 -0400

    update prime sieve

--- code/Math/prime.cpp
+++ code/Math/prime.cpp
@@ -1,9 +1,10 @@
-//Euler sieve
-int pri[MAXN],vis[MAXN],cnt=-1;
+//cnt is one greater than the number of primes
+// when traversing the pri array, use forn(i,cnt)
+int pri[N],vis[N],cnt=0;
 void init() {
-    for (int i = 2; i < MAXN; ++i) {
-        if (!vis[i]) pri[++cnt] = i;
-        for (int j = 0; j <= cnt; ++j) {
+    for (int i = 2; i < N; ++i) {
+        if (!vis[i]) pri[cnt++] = i;
+        for (int j = 0; j < cnt; ++j) {
             if (1ll * i * pri[j] >= MAXN) break;
             vis[i * pri[j]] = 1;
             if (i % pri[j]==0) break; 
