552594c9c14dd11a20852d297663a415d3cf7195
thallium <tuogengchen@gmail.com>
2019-10-28 07:32:51 -0400

    update prime sieve

--- code/Math/prime.cpp
+++ code/Math/prime.cpp
@@ -3,7 +3,7 @@ int pri[MAXN],vis[MAXN],cnt=-1;
 void init() {
     for (int i = 2; i < MAXN; ++i) {
         if (!vis[i]) pri[++cnt] = i;
-        for (int j = 0; j < cnt; ++j) {
+        for (int j = 0; j <= cnt; ++j) {
             if (1ll * i * pri[j] >= MAXN) break;
             vis[i * pri[j]] = 1;
             if (i % pri[j]==0) break; 
