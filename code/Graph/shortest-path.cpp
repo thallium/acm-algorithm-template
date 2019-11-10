46c7c3a13ca3c5faecbd3deb644b3124acaafe27
thallium <tuogengchen@gmail.com>
2019-11-10 17:07:30 -0500

    update shortest path

--- code/Graph/shortest-path.cpp
+++ code/Graph/shortest-path.cpp
@@ -1,46 +1,30 @@
-const int INF = 0x3f3f3f3f;
-const int MAXN = 1000010;
-struct qnode {
-    int v;
-    int c;
-    qnode(int _v = 0, int _c = 0): v(_v), c(_c){}
-    bool operator<(const qnode& r) const{ return c > r.c;}
-};
-struct Edge {
-    int v, cost;
-    Edge(int _v = 0, int _cost = 0): v(_v), cost(_cost){}
-};
-vector<Edge> E[MAXN];
-bool vis[MAXN];
-int dist[MAXN];
-//点的编号从 1 开始
-void Dijkstra(int n, int start){
-    memset(vis, false, sizeof(vis));
-    for (int i = 1; i <= n; i++)
-        dist[i] = INF;
-    priority_queue<qnode> que;
-    while (!que.empty())
-        que.pop();
-    dist[start] = 0;
-    que.push(qnode(start, 0));
-    qnode tmp;
-    while (!que.empty()) {
-        tmp = que.top();
-        que.pop();
-        int u = tmp.v;
-        if (vis[u])
-            continue;
-        vis[u] = true;
-        for (int i = 0; i < E[u].size(); i++) {
-            int v = E[tmp.v][i].v;
-            int cost = E[u][i].cost;
-            if (!vis[v] && dist[v] > dist[u] + cost) {
-                dist[v] = dist[u] + cost;
-                que.push(qnode(v, dist[v]));
-            }
-        }
-    }
-}
-void addedge(int u, int v, int w){
-    E[u].push_back(Edge(v, w));
+const int N=1e4+5;
+vector<pii> E[N];
+ll dis[N];
+bool vis[N];
+// vector<pii> pre[N];
+void dijkstra(int start){
+	ms(vis,false);
+	ms(dis,INF);
+	priority_queue<pii,vector<pii>,greater<pii>> q;
+	dis[start]=0;
+	q.push({0,start});
+	while(!q.empty()){
+		pii tmp=q.top();
+		q.pop();
+		int u=tmp.S;
+		if(vis[u]) continue;
+		vis[u]=1;
+		for(pii it:E[u]){
+			ll v=it.S,cost=it.F;
+			if(!vis[v]&&dis[v]>dis[u]+cost){
+				dis[v]=dis[u]+cost;
+				// pre[v].clear();
+				// pre[v].pb({cost,u});
+				q.push({dis[v],v});
+			}
+            // else if(dis[v]==dis[u]+cost)
+			// 	pre[v].pb({cost,u});
+		}
+	}
 }
