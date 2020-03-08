f741990220cb94d9cd53ef1d81b774ab474b9d18
Thallium <tuogengchen@gmail.com>
2020-03-08 12:37:26 -0400

    Update prime.cpp

--- code/Math/prime.cpp
+++ code/Math/prime.cpp
@@ -5,7 +5,7 @@ void init() {
     for (int i = 2; i < N; ++i) {
         if (!vis[i]) pri[cnt++] = i;
         for (int j = 0; j < cnt; ++j) {
-            if (1ll * i * pri[j] >= MAXN) break;
+            if (1ll * i * pri[j] >= N) break;
             vis[i * pri[j]] = 1;
             if (i % pri[j]==0) break; 
         }
