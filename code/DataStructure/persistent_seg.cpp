ba7ce22eea899b954ee5abf426ec87c2e648f043
gengchen <tuogengchen@gmail.com>
2020-04-18 13:02:49 -0400

    update persistent segtree

--- code/DataStructure/persistent_seg.cpp
+++ code/DataStructure/persistent_seg.cpp
@@ -1,45 +1,50 @@
 //find the nth biggest number
-const int N=1e5+5;
-struct node{
-    int l,r,val;
+struct PerSegTree {
+    vector<int> lson, rson, sum, root;
+    int tot;
+    PerSegTree(int n) {
+        lson = rson = sum = vector<int>(n << 5);
+        root = vector<int>(n + 1);
+        tot = 0;
+    }
+    void pushup(int rt) {
+        sum[rt] = sum[lson[rt]] + sum[rson[rt]];
+    }
+    void build(int l, int r, int& rt) {
+        rt = ++tot;
+        if (l == r) return;
+        int mid = (l + r) >> 1;
+        build(l, mid, lson[rt]);
+        build(mid + 1, r, rson[rt]);
+        pushup(rt);
+    }
+    void update(int pos, int val, int l, int r, int old, int& rt) {
+        rt = ++tot;
+        lson[rt] = lson[old];
+        rson[rt] = rson[old];
+        if (l == r) {
+            sum[rt] = sum[old] + val;
+            return;
+        }
+        int mid = (l + r) >> 1;
+        if (pos <= mid) update(pos, val, l, mid, lson[old], lson[rt]);
+        else update(pos, val, mid + 1, r, rson[old], rson[rt]);
+        pushup(rt);
+    }
+    int query(int l, int r, int old_rt, int rt) {
+        if(l==r) return //sth;
+        int mid=(l+r)>>1;
+        //sum[rt]-sum[old_rt] is the sum of the current interval
+    }
 };
-int ver[N];
-node tr[N*20];
-int cnt=0;
-void build(int& rt,int l,int r){
-    rt=++cnt;
-    tr[rt].val=0;
-    if(l==r) return;
-    int mid=(l+r)>>1;
-    build(tr[rt].l,l,mid);
-    build(tr[rt].r,mid+1,r);
-}
-void update(int& rt,int l,int r,int val){
-    tr[++cnt]=tr[rt];
-    rt=cnt;
-    tr[rt].val++;
-    if(l==r) return;
-    int mid=(l+r)>>1;
-    if(val<=mid) update(tr[rt].l,l,mid,val);
-    else update(tr[rt].r,mid+1,r,val);
-}
-int query(int rl,int rr,int l,int r,int val){
-    int d=tr[tr[rr].r].val-tr[tr[rl].r].val;
-    if(l==r) return l;
-    int mid=(l+r)>>1;
-    if(val<=d) return query(tr[rl].r,tr[rr].r,mid+1,r,val);
-    else return query(tr[rl].l,tr[rr].l,l,mid,val-d);
-}
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-	cnt=0;
-	build(ver[0],1,len);
-    forn(i,n){
+    PerSegTree tr;
+    tr.build(1,n,tr.root[0]);
+    for1(i,n){
 		int x;
 		cin>>x;
 		int pos=getpos(x);
-		update(ver[i],1,len,pos);
+        tr.update(pos,val,1,n,tr.root[i-1],tr.root[i]);
 	}
 	query(ver[l-1],ver[r],1,len,kth);
 }
--- code/Math/exGCD.cpp
+++ code/Math/exGCD.cpp
@@ -1,21 +1,21 @@
 // ax+by=gcd(a,b)
-int ex_gcd(int a, int b, int& x, int& y) {
+ll ex_gcd(ll a, ll b, ll& x, ll& y) {
   if (b == 0) {
     x = 1;
     y = 0;
     return a;
   }
-  int d = ex_gcd(b, a % b, x, y);
-  int temp = x;
+  ll d = ex_gcd(b, a % b, x, y);
+  ll temp = x;
   x = y;
   y = temp - a / b * y;
   return d;
 }
 //the smallest integer solution to ax+by=c;
-bool liEu(int a, int b, int c, int& x, int& y) {
-  int d = ex_gcd(a, b, x, y);
+bool liEu(ll a, ll b, ll c, ll& x, ll& y) {
+  ll d = ex_gcd(a, b, x, y);
   if (c % d != 0) return 0;
-  int k = c / d;
+  ll k = c / d;
   x *= k;
   y *= k;
   return 1;
