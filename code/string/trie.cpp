f3bc955552ca432fe5c59998e5bcb5ddc86ab604
gengchen <tuogengchen@gmail.com>
2020-04-20 19:53:28 -0400

    update trie

--- code/string/trie.cpp
+++ code/string/trie.cpp
@@ -1,23 +1,25 @@
-struct trie {
-  int nex[100000][26], cnt;
-  bool exist[100000];  // if this node is a end of a word
+struct Trie {
+    int child[100000][26], cnt=0;
+    bool exist[100000];  // if this node is a end of a word
 
-  void insert(char *s, int l) {  
-    int p = 0;
-    for (int i = 0; i < l; i++) {
-      int c = s[i] - 'a';
-      if (!nex[p][c]) nex[p][c] = ++cnt;  // if the node DNE, add it
-      p = nex[p][c];
+    void insert(string s) {  
+        int len=s.size();
+        int p = 0;
+        for (int i = 0; i < len; i++) {
+            int c = s[i] - 'a';
+            if (!child[p][c]) child[p][c] = ++cnt;  // if the node DNE, add it
+            p = child[p][c];
+        }
+        exist[p] = 1;
     }
-    exist[p] = 1;
-  }
-  bool find(char *s, int l) {  
-    int p = 0;
-    for (int i = 0; i < l; i++) {
-      int c = s[i] - 'a';
-      if (!nex[p][c]) return 0;
-      p = nex[p][c];
+    bool find(string s) {  
+        int len=s.size();
+        int p = 0;
+        for (int i = 0; i < len; i++) {
+            int c = s[i] - 'a';
+            if (!nex[p][c]) return 0;
+            p = nex[p][c];
+        }
+        return exist[p];
     }
-    return exist[p];
-  }
 };
