2477709b0ebeca86b57b403e3f2c3fe5b8d094d8
gengchen <tuogengchen@gmail.com>
2020-04-25 13:55:36 -0400

    add non-recursive-segtree

---
+++ code/DataStructure/segtree_none_recursive.cpp
@@ -0,0 +1,25 @@
+struct SegTree{
+    int n;
+    vector<int> t;
+    SegTree(int n_):n(n_){ t=vector<int>(2*n); }
+    SegTree(vector<int> a){
+        n=a.size();
+        t=vector<int>(2*n);
+        for (int i=0;i<n;i++) t[n+i]=a[i];
+        for (int i = n - 1; i > 0; --i) t[i] = t[i<<1] + t[i<<1|1];
+    }
+
+    void modify(int p, int value) {  // set value at position p
+        t[p += n] = value;
+        for (; p > 1; p >>= 1) t[p>>1] = t[p] + t[p^1];
+    }
+
+    int query(int l, int r) {  // sum on interval [l, r)
+        int res = 0;
+        for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+            if (l&1) res += t[l++];
+            if (r&1) res += t[--r];
+        }
+        return res;
+    }
+};
