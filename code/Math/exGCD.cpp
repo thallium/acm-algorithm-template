7285c8586d819fe44a592634eef4e3f2cf51defb
thallium <tuogengchen@gmail.com>
2020-12-27 15:30:13 -0500

    update ex_gcd

--- code/Math/exGCD.cpp
+++ code/Math/exGCD.cpp
@@ -1,22 +1,27 @@
-// ax+by=gcd(a,b)
-ll ex_gcd(ll a, ll b, ll& x, ll& y) {
-  if (b == 0) {
-    x = 1;
-    y = 0;
-    return a;
-  }
-  ll d = ex_gcd(b, a % b, x, y);
-  ll temp = x;
-  x = y;
-  y = temp - a / b * y;
-  return d;
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+//   {g, x, y}: ax+by=gcd(a,b)
+tuple<ll, ll, ll> exgcd(ll a, ll b) {
+    if (b==0) return {a, 1, 0};
+    auto [g, x, y]=exgcd(b, a%b);
+    return {g, y, x-a/b*y};
 }
-//the smallest integer solution to ax+by=c;
+
+/*
+solve ax+by=c, equivalently ax=c (mod b)
+all solutions: x=x0+b/g*t, y=y0-a/g*t
+smallest positive x=(x0%t+t)%t, where t=b/g
+*/
 bool liEu(ll a, ll b, ll c, ll& x, ll& y) {
-  ll d = ex_gcd(a, b, x, y);
-  if (c % d != 0) return 0;
-  ll k = c / d;
-  x *= k;
-  y *= k;
-  return 1;
+    ll g;
+    tie(g, x, y)=exgcd(a, b);
+    if (c % g != 0) return false;
+    ll k = c / g;
+    x *= k;
+    y *= k;
+    // smallest positive x:
+    // b/=g;
+    // x=(x%b+b)%b;
+    return true;
 }
