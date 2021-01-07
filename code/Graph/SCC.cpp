407f941cb0a5ff3e006901c69258a013ee20137a
thallium <tuogengchen@gmail.com>
2021-01-07 11:07:18 -0500

    rewrite tarjan SCC

--- code/Graph/SCC.cpp
+++ code/Graph/SCC.cpp
@@ -1,40 +1,49 @@
 #include<bits/stdc++.h>
-
 using namespace std;
 
-const int INF = 0x3f3f3f3f;
-typedef long long ll;
+struct SCC {
+    int n, pos=0;
+    vector<vector<int>> g;
+    vector<bool> vis;
+    vector<int> low, ord, stk;
+    vector<vector<int>> comp;
+    SCC(int _n) : n(_n), g(n), vis(n), low(n), ord(n, -1) {}
 
-int n,m;
-const int N=1e4+5;
-vector<int> vec[N];//graph
-int id=1,scc=0;
-int ids[N],low[N];//vevrtices with same low value are in the same SCC
-bool onstack[N];
-int stk[N],top=-1;
-int out[N];
-void dfs(int x){
-    stk[++top]=x;
-    onstack[x]=1;
-    ids[x]=low[x]=id++;
-    forn(i,vec[x].size()){
-        int to=vec[x][i];
-        if(ids[to]==-1) dfs(to);
-        if(onstack[to]) low[x]=min(low[to],low[x]);
+    void add_edge(int u, int v) {
+        g[u].push_back(v);
     }
-    if(ids[x]==low[x]){
-        while(top>-1){
-            int node=stk[top--];
-            onstack[node]=0;
-            low[node]=ids[x];
-            if(node==x) break;
+
+    void dfs(int u) {
+        low[u]=ord[u]=pos++;
+        stk.push_back(u);
+        vis[u]=true;
+        for (auto v : g[u]) {
+            if (ord[v]==-1) {
+                dfs(v);
+                low[u]=min(low[u], low[v]);
+            } else if (vis[v]) {
+                low[u]=min(low[u], ord[v]);
+            }
         }
-        scc++;
+
+        if (low[u]==ord[u]) {
+            comp.emplace_back();
+            while (true) {
+                int v=stk.back();
+                stk.pop_back();
+                vis[v]=false;
+                comp.back().push_back(v);
+                if (u==v) break;
+            }
+        }
     }
-}
-void tarjan(){
-    for1(i,n) ids[i]=-1;
-    for1(i,n){
-        if(ids[i]==-1) dfs(i);
+
+    void solve() {
+        for (int i=0; i<n; i++) {
+            if (ord[i]==-1) dfs(i);
+        }
+        // reverse(comp.begin(), comp.end()); if you need to make sure that for
+        // each edge (a, b), the component that contains b cannot be earlier
+        // than the component that contains a
     }
-}
+};
