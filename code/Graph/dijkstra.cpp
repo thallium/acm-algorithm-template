7914af9b4da84043a0ad3a0d3d8bafda1c258d8f
thallium <tuogengchen@gmail.com>
2021-01-16 21:46:53 -0500

    update dijkstra and max flow

--- code/Graph/dijkstra.cpp
+++ code/Graph/dijkstra.cpp
@@ -1,22 +1,19 @@
 // vector<pii> pre[N];
-void dijkstra(int start){
-    fill(dis.begin(), dis.end(), 1e9);
-    priority_queue<pii,vector<pii>,greater<pii>> q;
-    dis[start]=0;
-    q.push({0,start});
-    while(!q.empty()){
-        auto [d, u]=q.top();
-        q.pop();
-        if(d!=dis[u]) continue;
-        for(auto [cost,v]:g[u]){
-            if(dis[v]>dis[u]+cost){
-                dis[v]=dis[u]+cost;
-                // pre[v].clear();
-                // pre[v].pb({cost,u});
-                q.push({dis[v],v});
-            }
-            // else if(dis[v]==dis[u]+cost)
-            // 	pre[v].pb({cost,u});
-        }
+priority_queue<pii, vector<pii>, greater<pii>> q;
+dis[start] = 0;
+q.push({0, start});
+while (!q.empty()) {
+  auto [d, u] = q.top();
+  q.pop();
+  if (d != dis[u]) continue;
+  for (auto [v, cost] : g[u]) {
+    if (dis[v] > dis[u] + cost) {
+      dis[v] = dis[u] + cost;
+      // pre[v].clear();
+      // pre[v].pb({cost,u});
+      q.push({dis[v], v});
     }
+    // else if(dis[v]==dis[u]+cost)
+    // 	pre[v].pb({cost,u});
+  }
 }
--- code/Graph/max_flow.cpp
+++ code/Graph/max_flow.cpp
@@ -1,78 +1,64 @@
 // indexed from 0!
-struct Dinic {
+#include <bits/stdc++.h>
+struct Flow {
+    static constexpr int INF = 1e9;
+    int n;
     struct Edge {
-        int from, to, cap, flow;
-        Edge(int u, int v, int c, int f) : from(u), to(v), cap(c), flow(f) {}
+        int to, cap;
+        Edge(int to, int cap) : to(to), cap(cap) {}
     };
-    int n, s, t;
-    vector<Edge> edges;
-    vector<vector<int>> g;
-    vector<int> d,cur;
-    vector<int> vis;
-
-    Dinic(int _n) : n(_n), g(n), d(n), cur(n), vis(n){
-        edges.clear();
-    }
-
-    void init(int n) {
-        for (int i = 0; i < n; i++) g[i].clear();
-        edges.clear();
-    }
-
-    void AddEdge(int from, int to, int cap) {
-        edges.push_back(Edge(from, to, cap, 0));
-        edges.push_back(Edge(to, from, 0, 0));
-        int m = (int) edges.size();
-        g[from].push_back(m - 2);
-        g[to].push_back(m - 1);
-    }
-
-    bool BFS() {
-        fill(vis.begin(), vis.end(), 0);
-        queue<int> Q;
-        Q.push(s);
-        d[s] = 0;
-        vis[s] = 1;
-        while (!Q.empty()) {
-            int x = Q.front();
-            Q.pop();
-            for (int i = 0; i < (int) g[x].size(); i++) {
-                Edge& e = edges[g[x][i]];
-                if (!vis[e.to] && e.cap > e.flow) {
-                    vis[e.to] = 1;
-                    d[e.to] = d[x] + 1;
-                    Q.push(e.to);
+    std::vector<Edge> e;
+    std::vector<std::vector<int>> g;
+    std::vector<int> cur, h;
+    Flow(int n) : n(n), g(n) {}
+    bool bfs(int s, int t) {
+        h.assign(n, -1);
+        std::queue<int> que;
+        h[s] = 0;
+        que.push(s);
+        while (!que.empty()) {
+            int u = que.front();
+            que.pop();
+            for (int i : g[u]) {
+                auto [v, c] = e[i];
+                if (c > 0 && h[v] == -1) {
+                    h[v] = h[u] + 1;
+                    if (v == t) return true;
+                    que.push(v);
                 }
             }
         }
-        return vis[t];
+        return false;
     }
-
-    int DFS(int x, int a) {
-        if (x == t || a == 0) return a;
-        int flow = 0, f;
-        for (int& i = cur[x]; i < (int) g[x].size(); i++) {
-            Edge& e = edges[g[x][i]];
-            if (d[x] + 1 == d[e.to] && (f = DFS(e.to, min(a, e.cap - e.flow))) > 0) {
-                e.flow += f;
-                edges[g[x][i] ^ 1].flow -= f;
-                flow += f;
-                a -= f;
-                if (a == 0) break;
+    int dfs(int u, int t, int f) {
+        if (u == t)
+            return f;
+        int r = f;
+        for (int &i = cur[u]; i < int(g[u].size()); ++i) {
+            int j = g[u][i];
+            auto [v, c] = e[j];
+            if (c > 0 && h[v] == h[u] + 1) {
+                int a = dfs(v, t, std::min(r, c));
+                e[j].cap -= a;
+                e[j ^ 1].cap += a;
+                r -= a;
+                if (r == 0) return f;
             }
         }
-        return flow;
+        return f - r;
     }
-
-    int Maxflow(int s, int t) {
-        this->s = s;
-        this->t = t;
-        int flow = 0;
-        while (BFS()) {
-            const int INF=1e9;
-            fill(cur.begin(), cur.end(), 0);
-            flow += DFS(s, INF);
+    void addEdge(int u, int v, int c) {
+        g[u].push_back(e.size());
+        e.emplace_back(v, c);
+        g[v].push_back(e.size());
+        e.emplace_back(u, 0);
+    }
+    int maxFlow(int s, int t) {
+        int ans = 0;
+        while (bfs(s, t)) {
+            cur.assign(n, 0);
+            ans += dfs(s, t, INF);
         }
-        return flow;
+        return ans;
     }
 };
