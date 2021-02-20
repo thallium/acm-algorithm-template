f6af4448769c1b0ffb070f25a04c70eb5ed5003e
thallium <tuogengchen@gmail.com>
2021-02-19 20:48:09 -0500

    add find negative cycle in bellman ford

--- code/Graph/BellmanFord.cpp
+++ code/Graph/BellmanFord.cpp
@@ -1,11 +1,12 @@
 struct BellmanFord {
     static constexpr long long INF=1e18;
-    int n;
+    int n, last_relaxed=-1;
     vector<tuple<int, int, int>> edges;
     vector<bool> bad; //has negative cycle on the path
+    vector<int> pre;
     vector<ll> dis;
 
-    BellmanFord(int _n) : n(_n), bad(n), dis(n, INF) {}
+    BellmanFord(int _n) : n(_n), bad(n), pre(n), dis(n, INF) {}
 
     void add_edge(int u, int v, int w) {
         edges.emplace_back(u, v, w);
@@ -13,17 +14,21 @@ struct BellmanFord {
 
     void run(int start) {
         dis[start]=0;
-        for (int i=0; i<n; i++) {
+        for (int i=0; i<n-1; i++) {
             for (auto [u, v, w] : edges) {
-                if (dis[u]<INF) {
-                    dis[v]=min(dis[v], dis[u]+w);
+                if (dis[u]<INF && dis[v]>dis[u]+w) {
+                    dis[v]=dis[u]+w;
+                    pre[v]=u;
                 }
             }
         }
 
         for (auto [u, v, w] : edges) {
-            if (dis[u]<INF) {
-                if (dis[v]>dis[u]+w) bad[v]=true;
+            if (dis[u]<INF && dis[v]>dis[u]+w) {
+                dis[v]=dis[u]+w;
+                bad[v]=true;
+                last_relaxed=v;
+                pre[v]=u;
             }
         }
         for (int i=0; i<n; i++) {
@@ -31,6 +36,21 @@ struct BellmanFord {
                 if (bad[u]) bad[v]=true;
             }
         }
+    }
+
+    vector<int> find_cycle() {
+        dis.assign(n, 0); // without this, only cycle reachable from 0 will be counted
+        run(0);
+        if (last_relaxed==-1) return {};
+        int x=last_relaxed;
+        for (int i=0; i<n; i++) x=pre[x];
+        vector<int> cycle;
+        for (int cur=x; ; cur=pre[cur]) {
+            cycle.push_back(cur);
+            if (cur==x && cycle.size()>1) break;
+        }
+        reverse(cycle.begin(), cycle.end());
+        return cycle;
     }
 
     long long get_dis(int x) {
