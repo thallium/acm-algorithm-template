a6eed75ad8ad0278ae6c7ec6e7b395b9a7092056
thallium <tuogengchen@gmail.com>
2021-04-16 01:09:55 -0400

    refactor lca

---
+++ code/Graph/binary_lifting.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Binary_lifting {
+    int sz, level;
+    const vector<vector<int>>& g;
+    vector<vector<int>> pa;
+    vector<int> dep;
+
+    Binary_lifting(const vector<vector<int>>& g_) :
+        sz((int)g_.size()),
+        level(__lg((int)g_.size())+1),
+        pa(sz, vector<int>(level)),
+        g(g_),
+        dep(g.size()) {}
+
+    void dfs(int u, int p) {
+        pa[u][0] = p;
+        dep[u] = dep[p] + 1;
+        for (int i = 1; i < 20; i++) {
+            pa[u][i] = pa[pa[u][i - 1]][i - 1];
+        }
+        for (auto v : g[u]) {
+            if (v == p) continue;
+            dfs(v, u);
+        }
+    };
+
+    int jump(int u, int step) {
+        for (int i=0; i<level; i++) {
+            if (step>>i&1) u=pa[u][i];
+        }
+        return u;
+    }
+
+    int lca(int x, int y) {
+        if (dep[x] > dep[y]) swap(x, y);
+        y=jump(y, dep[y] - dep[x]);
+        if (x == y) return x;
+        for (int i=level-1; i>=0; i--) {
+            if (pa[x][i] != pa[y][i]) {
+                x = pa[x][i];
+                y = pa[y][i];
+            }
+        }
+        return pa[x][0];
+    }
+};
+
