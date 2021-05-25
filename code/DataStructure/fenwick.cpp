30df7ff070f8105794ca3ff2e8ea7e6da0261b97
thallium <tuogengchen@gmail.com>
2021-05-24 23:08:18 -0400

    update

--- code/DataStructure/fenwick.cpp
+++ code/DataStructure/fenwick.cpp
@@ -1,146 +1,70 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T>
-struct fenwick{
+template <typename T> struct fenwick {
+  int n;
+  vector<T> t;
+
+  fenwick(int n_) : n(n_), t(n) {}
+  fenwick(const vector<int> &v) : fenwick((int)v.size()) {
+    for (int i = 0; i < n; i++) {
+      t[i] += v[i];
+      int j = i | (i + 1);
+      if (j < n)
+        t[j] += t[i];
+    }
+  }
+
+  void update(int i, T x) {
+    while (i < n) {
+      t[i] += x;
+      i |= (i + 1);
+    }
+  }
+
+  template <typename U> U query(int i) {
+    U res{};
+    for (; i >= 0; i = (i & (i + 1)) - 1)
+      res += t[i];
+    return res;
+  }
+
+  template <typename U>
+  U query(int l, int r) { return query(r) - (l ? query(l - 1) : U{}); }
+
+  int search(T prefix) { // equivalent to upper_bound(prefix)
+    int pos = 0;
+    T sum = 0;
+    for (int i = 20; i >= 0; i--) {
+      if (pos + (1 << i) <= n && (sum + t[pos + (1 << i)] <= prefix)) {
+        pos += (1 << i);
+        sum += t[pos];
+      }
+    }
+    return pos + 1;
+  }
+};
+
+// fenwick tree with range update and range sum query
+template <typename T> struct fenwick_rg {
     int n;
-    vector<T> t;
+    vector<T> sum1, sum2;
+    fenwick_rg(int n_) : n(n_), sum1(n+1), sum2(n+1) {}
 
-    fenwick(int n_):n(n_), t(n+1) {}
-    fenwick(const vector<int>& v) : fenwick((int)v.size()) { 
-        for (int i=1;i<=n;i++) {
-            t[i]+=v[i-1]; int j=i+(i&-i); if(j<=n) t[j]+=t[i];
-        }
+    void update(int p, T x) {
+        p++;
+        for (int i = p; i <= n; i += i & -i)
+            sum1[i] += x, sum2[i] += x * p;
     }
+    void update(int l, int r, T x) { update(l, x), update(r + 1, -x); }
 
-    void update(int i,int x){
-        for(;i<=n;i+=i&-i){
-            t[i]+=x;
-        }
-    }
-
-    T query(int i){
-        T res=0;
-        for(;i>0;i-=i&-i) res+=t[i];
+    T query(int p) {
+        p++;
+        T res{};
+        for (int i = p; i; i -= i & -i)
+            res += (p + 1) * sum1[i] - sum2[i];
         return res;
     }
 
-    T query(int l,int r){
-        return query(r)-query(l-1);
-    }
-    int search(T prefix){//equivalent to upper_bound(prefix)
-        int pos=0;
-        T sum=0;
-        for(int i=20;i>=0;i--){
-            if(pos+(1<<i)<=n&&(sum+t[pos+(1<<i)]<=prefix)){
-                pos+=(1<<i);
-                sum+=t[pos];
-            }
-        }
-        return pos+1;
-    }
-};
-
-//fenwick tree with range update and range sum query
-struct fenwick{
-    int n;
-    vector<ll> sum1,sum2;
-    fenwick(int n_):n(n_),sum1(n),sum2(n){}
-
-    void update(int p, ll x){
-        for(int i=p;i<=n;i+=i&-i) sum1[i] += x, sum2[i] += x * p;
-    }
-    void update(int l,int r, ll x){
-        update(l, x), update(r + 1, -x);
-    }
-    ll query(int p){
-        ll res = 0;
-        for(int i=p;i;i-=i&-i) res += (p + 1) * sum1[i] - sum2[i];
-        return res;
-    }
-    ll query(int l,int r){
-        return query(r)-query(l-1);
-    }
-};
-// two dimensional, single update, range query
-struct fenwick{
-    int n;
-    vector<vector<int>> tree;
-    fenwick(int n_)tree(n+1,vector<int>(n+1)){}
-    void add(int x, int y, int z){ 
-        int memo_y = y;
-        while(x <= n){
-            y = memo_y;
-            while(y <= n) tree[x][y] += z, y += y & -y;
-            x += x & -x;
-        }
-    }
-    void ask(int x, int y){
-        int res = 0, memo_y = y;
-        while(x){
-            y = memo_y;
-            while(y)
-                res += tree[x][y], y -= y & -y;
-            x -= x & -x;
-        }
-    }
-};
-// 2D, range update, single query
-struct fenwick{
-    void add(int x, int y, int z){ 
-        int memo_y = y;
-        while(x <= n){
-            y = memo_y;
-            while(y <= n)
-                tree[x][y] += z, y += y & -y;
-            x += x & -x;
-        }
-    }
-    void range_add(int xa, int ya, int xb, int yb, int z){
-        add(xa, ya, z);
-        add(xa, yb + 1, -z);
-        add(xb + 1, ya, -z);
-        add(xb + 1, yb + 1, z);
-    }
-    void ask(int x, int y){
-        int res = 0, memo_y = y;
-        while(x){
-            y = memo_y;
-            while(y)
-                res += tree[x][y], y -= y & -y;
-            x -= x & -x;
-        }
-    }
-};
-// 2D, range update, range query
-struct fenwick{
-    ll t1[N][N], t2[N][N], t3[N][N], t4[N][N];
-    void add(ll x, ll y, ll z){
-        for(int X = x; X <= n; X += X & -X)
-            for(int Y = y; Y <= m; Y += Y & -Y){
-                t1[X][Y] += z;
-                t2[X][Y] += z * x;
-                t3[X][Y] += z * y;
-                t4[X][Y] += z * x * y;
-            }
-    }
-    void range_add(ll xa, ll ya, ll xb, ll yb, ll z){ //(xa, ya) 到 (xb, yb) 的矩形
-        add(xa, ya, z);
-        add(xa, yb + 1, -z);
-        add(xb + 1, ya, -z);
-        add(xb + 1, yb + 1, z);
-    }
-    ll ask(ll x, ll y){
-        ll res = 0;
-        for(int i = x; i; i -= i & -i)
-            for(int j = y; j; j -= j & -j)
-                res += (x + 1) * (y + 1) * t1[i][j]
-                    - (y + 1) * t2[i][j]
-                    - (x + 1) * t3[i][j]
-                    + t4[i][j];
-        return res;
-    }
-    ll range_ask(ll xa, ll ya, ll xb, ll yb){
-        return ask(xb, yb) - ask(xb, ya - 1) - ask(xa - 1, yb) + ask(xa - 1, ya - 1);
-    }
+    T query(int l, int r) { return query(r) - query(l - 1); }
 };
--- code/DataStructure/persistent_seg.cpp
+++ code/DataStructure/persistent_seg.cpp
@@ -1,50 +1,67 @@
 //find the nth biggest number
-struct PerSegTree {
-    vector<int> lson, rson, sum, root;
-    int tot;
-    PerSegTree(int n) {
-        lson = rson = sum = vector<int>(n << 5);
-        root = vector<int>(n + 1);
-        tot = 0;
+#include<bits/stdc++.h>
+using namespace std;
+struct PST {
+    int n, tot=0;
+    vector<int> lc, rc, sum, roots; // left child, right child
+    PST(int n_) : n(n_), lc(n<<5), rc(n<<5), sum(n<<5), roots(1) { // change the size to n<<6 if there are 2*n modification 
+        build(0, n-1, roots[0]); // the initial root node is 1!
     }
     void pushup(int rt) {
-        sum[rt] = sum[lson[rt]] + sum[rson[rt]];
+        sum[rt] = sum[lc[rt]] + sum[rc[rt]];
     }
     void build(int l, int r, int& rt) {
         rt = ++tot;
         if (l == r) return;
         int mid = (l + r) >> 1;
-        build(l, mid, lson[rt]);
-        build(mid + 1, r, rson[rt]);
+        build(l, mid, lc[rt]);
+        build(mid + 1, r, rc[rt]);
         pushup(rt);
     }
     void update(int pos, int val, int l, int r, int old, int& rt) {
         rt = ++tot;
-        lson[rt] = lson[old];
-        rson[rt] = rson[old];
+        lc[rt] = lc[old];
+        rc[rt] = rc[old];
         if (l == r) {
             sum[rt] = sum[old] + val;
             return;
         }
         int mid = (l + r) >> 1;
-        if (pos <= mid) update(pos, val, l, mid, lson[old], lson[rt]);
-        else update(pos, val, mid + 1, r, rson[old], rson[rt]);
+        if (pos <= mid) update(pos, val, l, mid, lc[old], lc[rt]);
+        else update(pos, val, mid + 1, r, rc[old], rc[rt]);
         pushup(rt);
     }
-    int query(int l, int r, int old_rt, int rt) {
-        if(l==r) return //sth;
-        int mid=(l+r)>>1;
-        //sum[rt]-sum[old_rt] is the sum of the current interval
+    int update(int pos, int val) { // return the root of the new version
+        int new_root;
+        update(pos, val, 0, n-1, roots.back(), new_root);
+        roots.push_back(new_root);
+        return new_root;
+    }
+    int query(int u, int v, int l, int r, int k) {
+        if (l==r) return l;
+        int mid=(l+r)/2, x=sum[lc[v]]-sum[lc[u]];
+        if (k<=x) return query(lc[u], lc[v], l, mid, k);
+        return query(rc[u], rc[v], mid+1, r, k-x);
     }
 };
 int main(){
-    PerSegTree tr;
-    tr.build(1,n,tr.root[0]);
-    for1(i,n){
-		int x;
-		cin>>x;
-		int pos=getpos(x);
-        tr.update(pos,val,1,n,tr.root[i-1],tr.root[i]);
-	}
-	query(ver[l-1],ver[r],1,len,kth);
+    int n, q;
+    cin>>n>>q;
+    vector<int> a(n);
+    for (auto& x : a) cin>>x;
+    auto comp=a;
+    sort(comp.begin(), comp.end());
+    comp.erase(unique(comp.begin(), comp.end()), comp.end());
+    PST tr(comp.size());
+    vector<int> roots(n+1);
+    roots[0]=1;
+    for (int i=0; i<n; i++) {
+        int p=lower_bound(comp.begin(), comp.end(), a[i])-comp.begin();
+        roots[i+1]=tr.update(p, 1);
+    }
+    while (q--) {
+        int l, r, k;
+        cin>>l>>r>>k;
+        cout<<comp[tr.query(roots[l-1], roots[r], 0, comp.size()-1, k)]<<'\n';
+    }
 }
