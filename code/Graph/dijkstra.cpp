e7c8ca5e38d5b5a60ec9615ccc5202b66677ff7d
thallium <tuogengchen@gmail.com>
2021-06-09 18:20:26 -0400

    add 1-k bfs

--- code/Graph/dijkstra.cpp
+++ code/Graph/dijkstra.cpp
@@ -1,9 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
 constexpr long long INF=1e18;
 vector<long long> dijkstra(const vector<vector<pair<int, int>>>& g, int start) {
     vector dis(g.size(), INF);
     // vector<pii> pre[N];
     using node=pair<long long, int>;
-    priority_queue<node, vector<node>, greater<node>> q;
+    priority_queue<node, vector<node>, greater<>> q;
     dis[start] = 0;
     q.emplace(0, start);
     while (!q.empty()) {
@@ -19,6 +21,29 @@ vector<long long> dijkstra(const vector<vector<pair<int, int>>>& g, int start) {
             }
             // else if(dis[v]==dis[u]+cost)
             // pre[v].pb({cost,u});
+        }
+    }
+    return dis;
+}
+
+// dijkstra for small edge weight (less than 10) aka 1-k bfs
+vector<int> SmallDijkstra(const vector<vector<pair<int, int>>>& g, int src, int lim) {
+    vector<vector<int>> qs(lim);
+    vector<int> dis(g.size(), -1);
+
+    dis[src] = 0; 
+    qs[0].push_back(src);
+    for (int d = 0, maxd = 0; d <= maxd; ++d) {
+        for (auto& q = qs[d % lim]; q.size(); ) {
+            int u = q.back();
+            q.pop_back();
+            if (dis[u] != d) continue;
+            for (auto [v, c] : g[u]) {
+                if (dis[v] != -1 && dis[v] <= d + c) continue;
+                dis[v] = d + c;
+                qs[(d + c) % lim].push_back(v);
+                maxd = max(maxd, d + c);
+            }
         }
     }
     return dis;
