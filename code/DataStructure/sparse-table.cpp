93330ec7a235e524db0154eedb45e6071f8fa792
thallium <tuogengchen@gmail.com>
2021-07-01 19:24:40 -0400

    improve sparse table

--- code/DataStructure/sparse-table.cpp
+++ code/DataStructure/sparse-table.cpp
@@ -1,19 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-template<typename T>
-struct sparse {
+template <typename T> struct sparse {
     int n, logn;
-    vector<vector<T>> f;
+    vector<vector<T>> v;
     function<T(T, T)> F;
-    sparse(const vector<int> &a, function<T(T, T)> func) : n((int)a.size()), logn(__lg(n)), f(n, vector<int>(logn+1, 0)), F(func) {
-        for (int i = 0; i < n; i++)
-            f[i][0] = a[i];
-        for (int j = 1; j <= logn; j++)
-            for (int i = 0; i + (1 << j) - 1 < n; i++)
-                f[i][j] = func(f[i][j - 1], f[i + (1 << (j - 1))][j - 1]);
+    sparse(const vector<int> &a, function<T(T, T)> func)
+        : n((int)a.size()), logn(__lg(n)), v(logn + 1, vector<T>(n + 1)), F(func) {
+        v[0] = a;
+        for (int i = 1; i <= logn; i++)
+            for (int j = 0; j + (1 << i) - 1 < n; j++)
+                v[i][j] = F(v[i - 1][j], v[i - 1][j + (1 << (i - 1))]);
     }
     int query(int x, int y) {
         int s = __lg(y - x + 1);
-        return F(f[x][s], f[y - (1 << s) + 1][s]);
+        return F(v[s][x], v[s][y - (1 << s) + 1]);
     }
 };
