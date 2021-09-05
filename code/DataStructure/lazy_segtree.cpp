3572fe141dae3110d7752ec7a89ebaaa5b5bc833
thallium <tuogengchen@gmail.com>
2021-09-04 19:42:39 -0600

    improve segtree

--- code/DataStructure/lazy_segtree.cpp
+++ code/DataStructure/lazy_segtree.cpp
@@ -1,71 +1,86 @@
 // lazy propagation
-template<typename T>
+#include<bits/stdc++.h>
+using namespace std;
 struct SegTree {
+    struct node {
+        int v=0; // don't forget to set default value (used for leaves), not necessarily zero element
+        int lazy=0;
+
+        void apply(int l, int r, int x) {
+            lazy += x;
+            v += (r - l + 1) * x;
+        }
+
+        bool has_lazy() { // used to check if need to propagate
+            return lazy!=0;
+        }
+
+        void clear_lazy() {
+            lazy=0;
+        }
+
+        node merge(const node& b) const {
+            node res;
+            res.v=v+b.v;
+            return res;
+        }
+    };
     int n;
-    vector<T> t;
+    vector<node> t;
     SegTree(int n_) : n(n_), t(4 * n) {}
-    template<typename U>
-    SegTree(const vector<U> &v) : SegTree((int)v.size()) {
-        build(1, 0, n - 1, v);
+    SegTree(int n_, int x) : SegTree(n_) {
+        build(1, 0, n - 1, [&](int i) { return x; });
     }
-    void pull(int node) { t[node] = t[node * 2] + t[node * 2 + 1]; }
-    template<typename U>
-    void build(int node, int l, int r, const vector<U> &v) {
+    SegTree(int n_, function<int(int)> f) : SegTree(n_) {
+        build(1, 0, n-1, f);
+    }
+    SegTree(const vector<int> &v) : SegTree((int)v.size()) {
+        build(1, 0, n - 1, [&](int i) { return v[i]; });
+    }
+    void pull(int node) { t[node] = t[node * 2].merge(t[node * 2 + 1]); }
+    void build(int node, int l, int r, function<int(int)> f) {
         if (l == r) {
-            return t[node].apply(l, r, v[l]);
+            return t[node].apply(l, r, f(l));
         }
         int mid = (l + r) / 2;
-        build(node * 2, l, mid, v);
-        build(node * 2 + 1, mid + 1, r, v);
+        build(node * 2, l, mid, f);
+        build(node * 2 + 1, mid + 1, r, f);
         pull(node);
     }
     void push(int p, int l, int r) {
-        if (t[p].lazy) {
+        if (t[p].has_lazy()) {
             int m = (l + r) / 2;
             t[p * 2].apply(l, m, t[p].lazy);
             t[p * 2 + 1].apply(m + 1, r, t[p].lazy);
-            t[p].lazy = 0;
+            t[p].clear_lazy();
         }
     }
     template<typename U>
-    void add(int node, int ql, int qr, int l, int r, U x) {
+    void update(int node, int ql, int qr, int l, int r, U x) {
         if (r < ql || l > qr) return;
         if (ql <= l && qr >= r) return t[node].apply(l, r, x);
         push(node, l, r);
         int mid = (l + r) / 2;
-        add(node * 2, ql, qr, l, mid, x);
-        add(node * 2 + 1, ql, qr, mid + 1, r, x);
+        update(node * 2, ql, qr, l, mid, x);
+        update(node * 2 + 1, ql, qr, mid + 1, r, x);
         pull(node);
     }
-    T get(int node, int ql, int qr, int l, int r) {
+    node get(int node, int ql, int qr, int l, int r) {
         if (ql <= l && qr >= r) return t[node];
         push(node, l, r);
         int mid = (l + r) / 2;
         if (qr <= mid) return get(node << 1, ql, qr, l, mid);
         if (ql > mid) return get(node << 1 | 1, ql, qr, mid+1, r);
-        return get(node << 1, ql, qr, l, mid) + get(node << 1 | 1, ql, qr, mid+1, r);
+        return get(node << 1, ql, qr, l, mid).merge(get(node << 1 | 1, ql, qr, mid+1, r));
     }
     // wrapper
     template <typename U>
     void add(int l, int r, U x) {
         assert(l >= 0 && l <= r && r < n);
-        add(1, l, r, 0, n-1, x);
+        update(1, l, r, 0, n-1, x);
     }
-    T get(int l, int r) {
+    node get(int l, int r) {
         assert(l >= 0 && l <= r && r < n);
         return get(1, l, r, 0, n-1);
-    }
-};
-struct node {
-    int v=0; // don't forget to set default value (used for leaves), not necessarily zero element
-    int lazy=0;
-    void apply(int l, int r, int x) {
-        lazy += x;
-        v += (r - l) * x;
-    }
-    node operator+(const node& b) const {
-        node res;
-        res.v=v+b.v;
-        return res;
     }
 };
