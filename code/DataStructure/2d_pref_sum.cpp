8049fcb11ad51a8b3d77aeec26bc9a94105cf4c0
Gengchen Tuo <tuogengchen@gmail.com>
2021-12-20 23:17:25 -0700

    add 2d prefix sum

---
+++ code/DataStructure/2d_pref_sum.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+template<typename T>
+struct pref_sum_2d {
+    int n, m;
+    vector<vector<T>> sum;
+    template<typename U>
+    pref_sum_2d(const vector<vector<U>>& a)
+        : n((int)a.size()), m((int)a[0].size()), sum(n+1, vector<T>(m+1)) {
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++) {
+                    sum[i+1][j+1]=a[i][j] + sum[i][j+1] + sum[i+1][j] - sum[i][j];
+                }
+        }
+    T query(int x1, int y1, int x2, int y2) {
+        return sum[x2+1][y2+1] - sum[x2+1][y1] - sum[x1][y2+1] + sum[x1][y1];
+    }
+};
