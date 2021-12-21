fca542c0ebf98058f48887aace67aa49a5b8339e
Gengchen Tuo <tuogengchen@gmail.com>
2021-12-20 23:17:07 -0700

    update 2d sparse table

--- code/DataStructure/sparse-table.cpp
+++ code/DataStructure/sparse-table.cpp
@@ -18,19 +18,17 @@ template <typename T> struct sparse {
     }
 };
 
-template<typename T, typename op> struct sparse_2d {
-    // using T = int;
+namespace st { // 2d sparse table
+    using T = int;
     int n, m, logn, logm;
-    // vector<vector<vector<vector<T>>>> t;
     static const int N = 1e3 + 5;
     T t[13][13][N][N]; // array layout matches loop order to ensure efficiency
 
-    sparse_2d(vector<vector<T>> val) {
+    template<typename U>
+    void init(const vector<vector<U>>& val) {
         n = ((int)val.size()), m = ((int)val[0].size()),
         logn = (__lg(n)), logm = (__lg(m));
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < m; j++)
-                t[0][0][i][j] = val[i][j];
+        for (int i = 0; i < n; i++) for (int j = 0; j < m; j++) t[0][0][i][j] = val[i][j];
         for (int i = 0; i <= logn; i++)
             for (int j = 0; j <= logm; j++) {
                 if (i == 0 && j == 0) continue;
@@ -38,19 +36,23 @@ template<typename T, typename op> struct sparse_2d {
                     for (int col = 0; col + (1 << j) - 1 < m; col++) {
                         // auto &v = t[row][col];
                         if (i == 0)
-                            t[i][j][row][col] = min(t[i][j - 1][row][col],
-                                          t[i][j - 1][row][col + (1 << (j - 1))]);
+                            t[i][j][row][col] = min(t[i][j - 1][row][col], t[i][j - 1][row][col + (1 << (j - 1))]);
                         if (j == 0)
-                            t[i][j][row][col] = min(t[i - 1][j][row][col],
-                                          t[i - 1][j][row + (1 << (i - 1))][col]);
+                            t[i][j][row][col] = min(t[i - 1][j][row][col], t[i - 1][j][row + (1 << (i - 1))][col]);
                         else
-                            t[i][j][row][col] = min(t[i][j - 1][row][col],
-                                          t[i][j - 1][row][col + (1 << (j - 1))]);
+                            t[i][j][row][col] = min(t[i][j - 1][row][col], t[i][j - 1][row][col + (1 << (j - 1))]);
                     }
                 }
             }
     }
     T query(int x1, int x2, int y1, int y2) {
+        assert(n!=0 && m!=0);
+        assert(x1 <= x2);
+        assert(y1 <= y2);
+        assert(x1 >= 0 && x1 < n);
+        assert(x2 >= 0 && x2 < n);
+        assert(y1 >= 0 && y1 < m);
+        assert(y2 >= 0 && y2 < m);
         int kx = __lg(x2 - x1 + 1), ky = __lg(y2 - y1 + 1);
         return min(
             {t[kx][ky][x1][y1], t[kx][ky][x2 - (1 << kx) + 1][y1],
