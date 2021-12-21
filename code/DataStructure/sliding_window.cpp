1f0e6fa0cd0d2ee7e612a95dbadc79f88bb0f5d5
Gengchen Tuo <tuogengchen@gmail.com>
2021-12-20 23:18:00 -0700

    add sliding window (monotonic queue)

---
+++ code/DataStructure/sliding_window.cpp
@@ -0,0 +1,13 @@
+template<typename T, typename compare = less<T>>
+struct sliding_window {
+    int k; // width of the window
+    deque<pair<int, T>> q;
+    compare cmp;
+    sliding_window(int k_) : k(k_), cmp() {}
+    void add(int i, T x) {
+        while (!q.empty() && !cmp(q.back().second, x)) q.pop_back();
+        q.emplace_back(i, x);
+        while (q.front().first <= i - k) q.pop_front();
+    }
+    T get() { return q.front().second; }
+};
