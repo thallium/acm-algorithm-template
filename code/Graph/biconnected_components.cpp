3c7880a62f3c9a8ecc73995409f52e972ca0167a
Gengchen Tuo <tuogengchen@gmail.com>
2021-12-20 23:18:41 -0700

    replace cut vertices with BCC which can do more

--- code/Graph/biconnected_components.cpp
+++ code/Graph/biconnected_components.cpp
@@ -1,28 +1,40 @@
-struct cut_vertex {
+#include <vector>
+using namespace std;
+struct BCC {
     int n, pos = 0;
     vector<vector<int>> g;
-    vector<int> ord, low, cuts;
-    cut_vertex(int n_) : n(n_), g(n), ord(n, -1), low(n) {}
+    vector<int> ord, low, cuts, stk;
+    vector<vector<int>> comps; // components
+    BCC(int n_) : n(n_), g(n), ord(n, -1), low(n) {}
     void add_edge(int u, int v) {
         g[u].push_back(v);
         g[v].push_back(u);
     }
     void dfs(int u, int pa) {
         low[u] = ord[u] = pos++;
-        int cnt = 0, sz = 1, sum = 0;
-        bool is_cut = 0;
+        stk.push_back(u);
+        int cnt=0;
+        bool is_cut = false;
         for (auto v : g[u]) {
             if (v == pa) continue;
             if (ord[v] == -1) {
                 cnt++;
                 dfs(v, u);
+                low[u] = min(low[u], low[v]);
                 if (low[v] >= ord[u]) {
                     if (u != pa || cnt > 1) is_cut = true;
                     // the subtree will be disconnected if we remove vertex u,
                     // do something if needed
+                    comps.emplace_back();
+                    while (true) {
+                        int back = stk.back();
+                        stk.pop_back();
+                        comps.back().push_back(back);
+                        if (back == v) break;
+                    }
+                    comps.back().push_back(u);
                 }
-            }
-            low[u] = min(low[u], low[v]);
+            } else low[u]=min(low[u], ord[v]);
         }
         if (is_cut) cuts.push_back(u);
     }
