e44f399b7cd920b4a279525b50fa61ddb13f77eb
Gengchen Tuo <tuogengchen@gmail.com>
2022-01-17 15:33:38 -0500

    update lazy segtree and factorial

--- code/DataStructure/lazy_segtree.cpp
+++ code/DataStructure/lazy_segtree.cpp
@@ -2,31 +2,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 struct SegTree {
-    struct node {
+    struct Node {
         int v=0; // don't forget to set default value (used for leaves), not necessarily zero element
         int lazy=0;
 
+        Node() = default;
+        explicit Node(int val) : v(val) {}
+
         void apply(int l, int r, int x) {
             lazy += x;
-            v += (r - l + 1) * x;
+            v += x;
         }
+        // used to check if need to propagate
+        bool has_lazy() { return lazy!=0; }
+        void clear_lazy() { lazy=0; }
 
-        bool has_lazy() { // used to check if need to propagate
-            return lazy!=0;
-        }
-
-        void clear_lazy() {
-            lazy=0;
-        }
-
-        node merge(const node& b) const {
-            node res;
-            res.v=v+b.v;
+        static Node merge(const Node& lhs, const Node& rhs) {
+            Node res;
+            res.v=min(lhs.v,rhs.v);
             return res;
         }
     };
     int n;
-    vector<node> t;
+    vector<Node> t;
     SegTree(int n_) : n(n_), t(4 * n) {}
     SegTree(int n_, int x) : SegTree(n_) {
         build(1, 0, n - 1, [&](int i) { return x; });
@@ -37,10 +35,11 @@ struct SegTree {
     SegTree(const vector<int> &v) : SegTree((int)v.size()) {
         build(1, 0, n - 1, [&](int i) { return v[i]; });
     }
-    void pull(int node) { t[node] = t[node * 2].merge(t[node * 2 + 1]); }
+    void pull(int node) { t[node] = Node::merge(t[node * 2], t[node * 2 + 1]); }
     void build(int node, int l, int r, function<int(int)> f) {
         if (l == r) {
-            return t[node].apply(l, r, f(l));
+            t[node]=Node{f(l)};
+            return;
         }
         int mid = (l + r) / 2;
         build(node * 2, l, mid, f);
@@ -65,21 +64,22 @@ struct SegTree {
         update(node * 2 + 1, ql, qr, mid + 1, r, x);
         pull(node);
     }
-    node get(int node, int ql, int qr, int l, int r) {
+    Node get(int node, int ql, int qr, int l, int r) {
         if (ql <= l && qr >= r) return t[node];
         push(node, l, r);
         int mid = (l + r) / 2;
         if (qr <= mid) return get(node << 1, ql, qr, l, mid);
         if (ql > mid) return get(node << 1 | 1, ql, qr, mid+1, r);
-        return get(node << 1, ql, qr, l, mid).merge(get(node << 1 | 1, ql, qr, mid+1, r));
+        return Node::merge(get(node << 1, ql, qr, l, mid), get(node << 1 | 1, ql, qr, mid+1, r));
     }
     // wrapper
     template <typename U>
     void add(int l, int r, U x) {
+        if (l==r+1) return; // empty interval, but also can be bug in code
         assert(l >= 0 && l <= r && r < n);
         update(1, l, r, 0, n-1, x);
     }
-    node get(int l, int r) {
+    Node get(int l, int r) {
         assert(l >= 0 && l <= r && r < n);
         return get(1, l, r, 0, n-1);
     }
