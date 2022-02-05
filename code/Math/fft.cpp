9e29ccf95616cf0f2c395520a5bb488f544e681a
Gengchen Tuo <tuogengchen@gmail.com>
2022-02-05 18:19:56 -0500

    add poly multiply to fft.cpp

--- code/Math/fft.cpp
+++ code/Math/fft.cpp
@@ -29,3 +29,23 @@ void fft(vector<cd>& a, bool invert) {
         for (cd & x : a) x /= n;
     }
 }
+
+vector<int> multiply(vector<int> const& a, vector<int> const& b) {
+    vector<cd> fa(a.begin(), a.end()), fb(b.begin(), b.end());
+    int n = 1;
+    while (n < a.size() + b.size()) 
+        n <<= 1;
+    fa.resize(n);
+    fb.resize(n);
+
+    fft(fa, false);
+    fft(fb, false);
+    for (int i = 0; i < n; i++) fa[i] *= fb[i];
+    fft(fa, true);
+
+    vector<int> result(n);
+    for (int i = 0; i < n; i++)
+        result[i] = round(fa[i].real());
+    return result;
+}
+
