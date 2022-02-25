72ef41193a3c32479bffeb4bd848a628beb2b01d
Gengchen Tuo <tuogengchen@gmail.com>
2022-02-25 15:36:56 -0500

    make mod int shorter

--- code/Math/mod_int.hpp
+++ code/Math/mod_int.hpp
@@ -1,48 +1,26 @@
 template <int MOD>
 struct ModInt {
     int val;
-    // constructor
-    ModInt(long long v = 0) : val(int(v % MOD)) {
-        if (val < 0) val += MOD;
-    };
-    // unary operator
+    ModInt(int v = 0) : val(v % MOD) { if (val < 0) val += MOD; };
     ModInt operator+() const { return ModInt(val); }
     ModInt operator-() const { return ModInt(MOD - val); }
-    ModInt inv() const { return this->pow(MOD - 2); }
-    // arithmetic
-    ModInt operator+(const ModInt& x) const { return ModInt(*this) += x; }
-    ModInt operator-(const ModInt& x) const { return ModInt(*this) -= x; }
-    ModInt operator*(const ModInt& x) const { return ModInt(*this) *= x; }
-    ModInt operator/(const ModInt& x) const { return ModInt(*this) /= x; }
-    ModInt pow(long long n) const {
-        auto x = ModInt(1);
-        auto b = *this;
-        while (n > 0) {
-            if (n & 1) x *= b;
-            n >>= 1;
-            b *= b;
-        }
-        return x;
+    ModInt inv() const {
+        auto a = val, m = MOD, u = 0, v = 1;
+        while (a != 0) { auto t = m / a; m -= t * a; swap(a, m); u -= t * v; swap(u, v); }
+        assert(m == 1);
+        return u;
     }
-    // compound assignment
-    ModInt& operator+=(const ModInt& x) {
-        if ((val += x.val) >= MOD) val -= MOD;
-        return *this;
-    }
-    ModInt& operator-=(const ModInt& x) {
-        if ((val -= x.val) < 0) val += MOD;
-        return *this;
-    }
-    ModInt& operator*=(const ModInt& x) {
-        val = int((long long)(val) * x.val % MOD);
-        return *this;
-    }
+    friend ModInt operator+ (ModInt lhs, const ModInt& rhs) { return lhs += rhs; }
+    friend ModInt operator- (ModInt lhs, const ModInt& rhs) { return lhs -= rhs; }
+    friend ModInt operator* (ModInt lhs, const ModInt& rhs) { return lhs *= rhs; }
+    friend ModInt operator/ (ModInt lhs, const ModInt& rhs) { return lhs /= rhs; }
+    ModInt& operator+=(const ModInt& x) { if ((val += x.val) >= MOD) val -= MOD; return *this; }
+    ModInt& operator-=(const ModInt& x) { if ((val -= x.val) < 0) val += MOD; return *this; }
+    ModInt& operator*=(const ModInt& x) { val = int64_t(val) * x.val % MOD; return *this; }
     ModInt& operator/=(const ModInt& x) { return *this *= x.inv(); }
-    // compare
     bool operator==(const ModInt& b) const { return val == b.val; }
     bool operator!=(const ModInt& b) const { return val != b.val; }
-    // I/O
     friend std::istream& operator>>(std::istream& is, ModInt& x) noexcept { return is >> x.val; }
     friend std::ostream& operator<<(std::ostream& os, const ModInt& x) noexcept { return os << x.val; }
 };
-using mint = ModInt<int(1e9+7)>;
+using mint = ModInt<1'000'000'007>;
