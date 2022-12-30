2df8f99ed1b495ca38cf9c0eea175e53b0f01804
Gengchen <tuogengchen@gmail.com>
2022-12-29 19:29:51 -0700

    change aug path to a function

--- src/Graph/augmented_path_BPM.hpp
+++ src/Graph/augmented_path_BPM.hpp
@@ -2,14 +2,11 @@
 // matching Worst time complexity: O(nm), but very hard to hack (since
 // we can shuffle), usually runs extremely fast, 2e5 vertices and
 // edges in 60 ms.
-#include <random>
 #include <vector>
-struct aug_path {
-    std::vector<std::vector<int>> g;
-    std::vector<int> L, R, vis;
-    aug_path(int n, int m) : g(n), L(n, -1), R(m, -1), vis(n) {}
-    void add_edge(int a, int b) { g[a].push_back(b); }
-    bool match(int u) {
+
+static auto aug_path(int n, int m, const std::vector<std::vector<int>> g) -> std::pair<int, std::vector<std::pair<int, int>>> {
+    std::vector<int> L(n, -1), R(m, -1), vis(n);
+    auto match = [&](auto& slf, int u) {
         if (vis[u]) return false;
         vis[u] = true;
         for (auto v : g[u]) {
@@ -19,45 +16,31 @@ struct aug_path {
                 return true;
             }
         }
-        for (auto vec : g[u]) {
-            if (match(R[vec])) {
-                L[u] = vec;
-                R[vec] = u;
+        for (auto v : g[u]) {
+            if (slf(slf, R[v])) {
+                L[u] = v;
+                R[v] = u;
                 return true;
             }
         }
         return false;
-    }
+    };
 
-    // shuffle to avoid counter test case, but may be slightly slower
-    template<bool to_shuffle = false>
-    int solve() {
-        std::vector<int> order;
-        if constexpr (to_shuffle) {
-            std::mt19937 rng(1);
-            for (auto& v : g)
-                shuffle(v.begin(), v.end(), rng);
-            order.resize(L.size());
-            iota(order.begin(), order.end(), 0);
-            shuffle(order.begin(), order.end(), rng);
+    bool ok = true;
+    while (ok) {
+        ok = false;
+        fill(vis.begin(), vis.end(), 0);
+        for (int i = 0; i < n; ++i) {
+            if (L[i] == -1) ok |= match(match, i);
         }
-        bool ok = true;
-        while (ok) {
-            ok = false;
-            fill(vis.begin(), vis.end(), 0);
-            if constexpr (to_shuffle) {
-                for (auto i : order) {
-                    if (L[i] == -1) ok |= match(i);
-                }
-            } else {
-                for (int i = 0; i < (int)L.size(); ++i) {
-                    if (L[i] == -1) ok |= match(i);
-                }
-            }
-        }
-        int ret = 0;
-        for (int i = 0; i < L.size(); ++i)
-            ret += (L[i] != -1);
-        return ret;
     }
-};
+    int cnt = 0;
+    std::vector<std::pair<int, int>> matches;
+    for (size_t i = 0; i < n; i++) {
+        if (L[i] != -1) {
+            cnt++;
+            matches.emplace_back(i, L[i]);
+        }
+    }
+    return {cnt, matches};
+}
