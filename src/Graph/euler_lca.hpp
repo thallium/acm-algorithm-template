b0a20c45f65a4b3b41456caf5ec7aefc149e3588
Gengchen <tuogengchen@gmail.com>
2023-04-18 23:40:35 -0400

    change euler_lca to use old sparse table

--- src/Graph/euler_lca.hpp
+++ src/Graph/euler_lca.hpp
@@ -1,6 +1,28 @@
 #include <vector>
-#include "data_structure/sparse-table.hpp"
+#include <functional>
+#include "misc/util.hpp"
 
+template <typename T> struct SparseTable {
+    size_t n, logn;
+    std::vector<std::vector<T>> v;
+    std::function<int(int, int)> F;
+    SparseTable() = default;
+    SparseTable(const std::vector<T> &a, std::function<int(int, int)> f)
+        : n(a.size()), logn(lg(n)), v(logn + 1, std::vector<T>(n)), F(f) {
+        v[0] = a;
+        for (size_t i = 1; i <= logn; i++)
+            for (size_t j = 0; j + (1 << i) - 1 < n; j++)
+                v[i][j] = F(v[i - 1][j], v[i - 1][j + (1 << (i - 1))]);
+    }
+    // get result for [l, r)
+    T query(size_t l, size_t r) {
+        assert(l < r);
+        assert(l < n);
+        assert(r <= n);
+        int s = lg(r - l);
+        return F(v[s][l], v[s][r - (1 << s)]);
+    }
+};
 struct EulerLCA {
     int n;
     std::vector<int> pos, seq, dep;
@@ -9,11 +31,14 @@ struct EulerLCA {
     EulerLCA(const std::vector<std::vector<int>>& g, int root) : n(g.size()), pos(n), dep(n) {
         seq.reserve(2 * n);
         dfs(root, root, g);
-        st = SparseTable<int, mn>(seq);
+        auto mn = [&](int u, int v) {
+            return pos[u] < pos[v] ? u : v;
+        };
+        st = SparseTable<int>(seq, mn);
     }
 
     void dfs(int u, int p, const std::vector<std::vector<int>>& g) {
-        pos[u] = seq.size();
+        pos[u] = (int)seq.size();
         seq.push_back(u);
         for (auto v : g[u]) {
             if (v == p) continue;
@@ -26,9 +51,5 @@ struct EulerLCA {
     int lca(int u, int v) {
         if (pos[u] > pos[v]) std::swap(u, v);
         return st.query(pos[u], pos[v] + 1);
-    }
-
-    int mn(int u, int v) {
-        return pos[u] < pos[v] ? u : v;
     }
 };
