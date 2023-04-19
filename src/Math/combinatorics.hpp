19fc0c64558ec29671140113cdf1a7a8024cc15f
Gengchen <tuogengchen@gmail.com>
2023-04-18 23:40:44 -0400

    update combi

--- src/Math/combinatorics.hpp
+++ src/Math/combinatorics.hpp
@@ -3,7 +3,7 @@
 template <typename mint>
 struct Combi {
     std::vector<mint> fac, invfac;
-    Combi(int n) : fac(n + 1), invfac(n + 1) {
+    constexpr Combi(const int n) : fac(n + 1), invfac(n + 1) {
         fac[0] = 1;
         for (int i = 1; i <= n; i++) {
             fac[i] = fac[i - 1] * i;
@@ -25,9 +25,14 @@ struct Combi {
         assert((int)size(fac) > n);
         return fac[n] * invfac[n - k] * invfac[k];
     }
-    mint P(int n, int m) { // n choose m with permutation
-        assert(!fac.empty());
-        return fac[n] * invfac[n - m];
+    mint P(int n, int k) { // n choose m with permutation
+        if (k < 0 || k > n) return 0;
+        assert((int)size(fac) > n);
+        return fac[n] * invfac[n - k];
+    }
+    mint catalan(int n) {
+        assert(2 * n < (int)size(fac));
+        return C(2 * n, n) - C(2 * n, n - 1);
     }
 
     // evaluate expressions consisting of multiplication and division
