7ad017bd5ad91de0c62f2a0e3e9bd4a44cd6116c
Gengchen <tuogengchen@gmail.com>
2023-04-27 22:24:59 -0400

    revert sparse table design so euler lca can use it easily

--- src/Graph/euler_lca.hpp
+++ src/Graph/euler_lca.hpp
@@ -1,40 +1,18 @@
+#pragma once
 #include <vector>
-#include <functional>
+#include "data_structure/sparse-table.hpp"
 #include "misc/util.hpp"
 
-template <typename T> struct SparseTable {
-    size_t n, logn;
-    std::vector<std::vector<T>> v;
-    std::function<int(int, int)> F;
-    SparseTable() = default;
-    SparseTable(const std::vector<T> &a, std::function<int(int, int)> f)
-        : n(a.size()), logn(lg(n)), v(logn + 1, std::vector<T>(n)), F(f) {
-        v[0] = a;
-        for (size_t i = 1; i <= logn; i++)
-            for (size_t j = 0; j + (1 << i) - 1 < n; j++)
-                v[i][j] = F(v[i - 1][j], v[i - 1][j + (1 << (i - 1))]);
-    }
-    // get result for [l, r)
-    T query(size_t l, size_t r) {
-        assert(l < r);
-        assert(l < n);
-        assert(r <= n);
-        int s = lg(r - l);
-        return F(v[s][l], v[s][r - (1 << s)]);
-    }
-};
 struct EulerLCA {
     int n;
     std::vector<int> pos, seq, dep;
     SparseTable<int> st;
 
-    EulerLCA(const std::vector<std::vector<int>>& g, int root) : n(g.size()), pos(n), dep(n) {
+    EulerLCA(const std::vector<std::vector<int>>& g, int root) : n((int)g.size()), pos(n), dep(n) {
         seq.reserve(2 * n);
         dfs(root, root, g);
-        auto mn = [&](int u, int v) {
-            return pos[u] < pos[v] ? u : v;
-        };
-        st = SparseTable<int>(seq, mn);
+        auto mn = [&](int u, int v) { return pos[u] < pos[v] ? u : v; };
+        st = SparseTable{seq, mn};
     }
 
     void dfs(int u, int p, const std::vector<std::vector<int>>& g) {
--- src/data_structure/sparse-table.hpp
+++ src/data_structure/sparse-table.hpp
@@ -1,19 +1,23 @@
+#pragma once
 #include <vector>
 #include <functional>
 #include "misc/util.hpp"
 
-template <typename T, auto F> struct SparseTable {
+template <typename T> struct SparseTable {
     size_t n, logn;
     std::vector<std::vector<T>> v;
+    std::function<T(T, T)> F;
+
     SparseTable() = default;
-    SparseTable(const std::vector<T> &a)
-        : n(a.size()), logn(lg(n)), v(logn + 1, std::vector<T>(n)) {
+    template<typename Func>
+    SparseTable(const std::vector<T> &a, Func f)
+        : n(a.size()), logn(lg(n)), v(logn + 1, std::vector<T>(n)), F(f) {
         v[0] = a;
         for (size_t i = 1; i <= logn; i++)
             for (size_t j = 0; j + (1 << i) - 1 < n; j++)
                 v[i][j] = F(v[i - 1][j], v[i - 1][j + (1 << (i - 1))]);
     }
-    // get result for [l, r)
+
     T query(size_t l, size_t r) {
         assert(l < r);
         assert(l < n);
