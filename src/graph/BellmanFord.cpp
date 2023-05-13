a856fe62834b8d6d3015f39881e05667d18e8219
Gengchen <tuogengchen@gmail.com>
2023-05-12 21:39:44 -0400

    format bellman ford

--- src/graph/BellmanFord.cpp
+++ src/graph/BellmanFord.cpp
@@ -1,53 +1,58 @@
+#pragma once
+
+#include <vector>
+#include <array>
 struct BellmanFord {
-    static constexpr long long INF=1e18;
-    int n, last_relaxed=-1;
-    vector<tuple<int, int, int>> edges;
-    vector<bool> bad; //has negative cycle on the path
-    vector<int> pre;
-    vector<ll> dis;
+    static constexpr long long INF = 1e18;
+    int n, last_relaxed = -1;
+    std::vector<std::array<int, 3>> edges;
+    std::vector<bool> bad; // has negative cycle on the path
+    std::vector<int> pre;
+    std::vector<long long> dis;
     BellmanFord(int _n) : n(_n), bad(n), pre(n), dis(n, INF) {}
-    void add_edge(int u, int v, int w) {
-        edges.emplace_back(u, v, w);
-    }
+    void add_edge(int u, int v, int w) { edges.emplace_back(u, v, w); }
     void run(int start) {
-        dis[start]=0;
-        for (int i=0; i<n-1; i++) {
+        dis[start] = 0;
+        for (int i = 0; i < n - 1; i++) {
             for (auto [u, v, w] : edges) {
-                if (dis[u]<INF && dis[v]>dis[u]+w) {
-                    dis[v]=dis[u]+w;
-                    pre[v]=u;
+                if (dis[u] < INF && dis[v] > dis[u] + w) {
+                    dis[v] = dis[u] + w;
+                    pre[v] = u;
                 }
             }
         }
         for (auto [u, v, w] : edges) {
-            if (dis[u]<INF && dis[v]>dis[u]+w) {
-                dis[v]=dis[u]+w;
-                bad[v]=true;
-                last_relaxed=v;
-                pre[v]=u;
+            if (dis[u] < INF && dis[v] > dis[u] + w) {
+                dis[v] = dis[u] + w;
+                bad[v] = true;
+                last_relaxed = v;
+                pre[v] = u;
             }
         }
-        for (int i=0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             for (auto [u, v, w] : edges) {
-                if (bad[u]) bad[v]=true;
+                if (bad[u])
+                    bad[v] = true;
             }
         }
     }
-    vector<int> find_cycle() {
-        dis.assign(n, 0); // without this, only cycle reachable from 0 will be counted
+    std::vector<int> find_cycle() {
+        dis.assign(n,
+                0); // without this, only cycle reachable from 0 will be counted
         run(0);
-        if (last_relaxed==-1) return {};
-        int x=last_relaxed;
-        for (int i=0; i<n; i++) x=pre[x];
-        vector<int> cycle;
-        for (int cur=x; ; cur=pre[cur]) {
+        if (last_relaxed == -1)
+            return {};
+        int x = last_relaxed;
+        for (int i = 0; i < n; i++)
+            x = pre[x];
+        std::vector<int> cycle;
+        for (int cur = x;; cur = pre[cur]) {
             cycle.push_back(cur);
-            if (cur==x && cycle.size()>1) break;
+            if (cur == x && cycle.size() > 1)
+                break;
         }
         reverse(cycle.begin(), cycle.end());
         return cycle;
     }
-    long long get_dis(int x) {
-        return bad[x] ? -INF : dis[x];
-    }
+    long long get_dis(int x) { return bad[x] ? -INF : dis[x]; }
 };
