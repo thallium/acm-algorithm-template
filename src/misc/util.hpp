ae4f7ba9a05b2c441a1af4c90af8f823cd065dc3
Gengchen <tuogengchen@gmail.com>
2023-05-15 21:55:55 -0400

    use bit library for c++20

--- src/misc/util.hpp
+++ src/misc/util.hpp
@@ -1,4 +1,16 @@
 #pragma once
+#if __cplusplus == 202002
+#include <bit>
+template<typename T>
+constexpr T lg(T x) {
+    return std::bit_width(x) - 1;
+}
+
+template<typename T>
+constexpr T ceil_lg(T x) {
+    return std::countr_zero(std::bit_ceil(x));
+}
+#else
 constexpr unsigned lg(int x) {
     return sizeof(int) * 8 - 1 - __builtin_clz(x);
 }
@@ -21,3 +33,4 @@ constexpr unsigned lg(unsigned long long x) {
 constexpr unsigned ceil_lg(int n) {
     return n == 0 ? 0 : 32 - __builtin_clz(n - 1);
 }
+#endif
