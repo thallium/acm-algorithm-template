2c3e0306f46daa4226efb2d5a6e196d53f6c2b74
Gengchen <tuogengchen@gmail.com>
2023-05-22 12:49:44 -0400

    rewrite manacher

---
+++ src/string/manacher.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <array>
+#include <vector>
+
+// return [even, odd]
+template <typename T>
+std::array<std::vector<int>, 2> manacher(const T& s) {
+    int n = (int)size(s);
+    std::array d{std::vector<int>(n + 1), std::vector<int>(n)};
+
+    for (int z : {0, 1}) {
+        auto& p = d[z];
+        for (int i = 0, l = 0, r = 0; i < n; i++) {
+            int t = r - i + !z;
+            if (i < r) {
+                p[i] = std::min(t, p[l + t]);
+            }
+            int l2 = i - p[i], r2 = i + p[i] - !z;
+            while (l2 && r2 + 1 < n && s[l2 - 1] == s[r2 + 1]) {
+                ++p[i];
+                --l2, ++r2;
+            }
+            if (r2 > r) {
+                l = l2, r = r2;
+            }
+        }
+    }
+    return d;
+}
