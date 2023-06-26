fe27a5250cbb8e3bdcb0816c553ed3a5a07efc14
Gengchen <tuogengchen@gmail.com>
2023-06-26 14:50:31 -0400

    seperate out range fenwick tree

--- src/data_structure/fenwick.hpp
+++ src/data_structure/fenwick.hpp
@@ -66,37 +66,3 @@ template <typename T> struct Fenwick {
         return partition_point([&](auto x) { return x <= target; });
     }
 };
-// fenwick tree with range update and range sum query
-struct fenwick_rg {
-    int n;
-    std::vector<int64_t> sum1, sum2;
-    fenwick_rg(int n_) : n(n_), sum1(n + 1), sum2(n + 1) {}
-
-  private:
-    void add(int i, int x) {
-        assert(i >= 0 && i < n);
-        i++;
-        int64_t v = (int64_t)i * x;
-        for (; i <= n; i += i & -i)
-            sum1[i] += x, sum2[i] += v;
-    }
-
-  public:
-    void add(int l, int r, int x) {
-        assert(l >= 0 && l <= r && r < n);
-        add(l, x);
-        if (r + 1 < n) add(r + 1, -x);
-    }
-    int64_t query(int p) {
-        assert(p >= 0 && p < n);
-        p++;
-        int64_t res{};
-        for (int i = p; i; i -= i & -i)
-            res += (p + 1) * sum1[i] - sum2[i];
-        return res;
-    }
-    int64_t query(int l, int r) {
-        assert(l >= 0 && l <= r && r < n);
-        return query(r) - (l ? query(l - 1) : 0);
-    }
-};
---
+++ src/data_structure/fenwick_range_update.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include <vector>
+
+// fenwick tree with range update and range sum query
+class fenwick_rg {
+    int n;
+    std::vector<int64_t> sum1, sum2;
+    fenwick_rg(int n_) : n(n_), sum1(n + 1), sum2(n + 1) {}
+
+    void add(int i, int x) {
+        assert(i >= 0 && i < n);
+        i++;
+        int64_t v = (int64_t)i * x;
+        for (; i <= n; i += i & -i)
+            sum1[i] += x, sum2[i] += v;
+    }
+
+public:
+    // [l, r)
+    void add(int l, int r, int x) {
+        assert(l >= 0 && l < r && r <= n);
+        add(l, x);
+        if (r < n)
+            add(r, -x);
+    }
+    int64_t get(int p) {
+        assert(p >= 0 && p <= n);
+        int64_t res{};
+        for (int i = p; i; i -= i & -i)
+            res += (p + 1) * sum1[i] - sum2[i];
+        return res;
+    }
+
+    // [l, r)
+    int64_t get(int l, int r) {
+        assert(l >= 0 && l < r && r <= n);
+        return get(r) - get(l);
+    }
+};
