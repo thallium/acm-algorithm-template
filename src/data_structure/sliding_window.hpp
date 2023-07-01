c6d468188d6dc7d3aeae04abc491c6bd4c749514
Gengchen <tuogengchen@gmail.com>
2023-06-30 21:47:53 -0400

    make sliding window hpp

--- src/data_structure/sliding_window.hpp
+++ src/data_structure/sliding_window.hpp
@@ -1,12 +1,14 @@
+#pragma once
+
 #include <functional>
 #include <queue>
 
 template<typename T, typename compare = std::less<T>>
-struct sliding_window {
-    int k; // width of the window
+struct SlidingWindow {
+    const int k; // width of the window
     std::deque<std::pair<int, T>> q;
     compare cmp;
-    sliding_window(int k_) : k(k_), cmp() {}
+    explicit SlidingWindow(int k_) : k(k_), cmp() {}
     void add(int i, T x) {
         while (!q.empty() && !cmp(q.back().second, x)) q.pop_back();
         q.emplace_back(i, x);
