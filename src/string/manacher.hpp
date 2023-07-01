b082b0ef60f71ae76dd95290b15909bf35466df0
Gengchen <tuogengchen@gmail.com>
2023-07-01 18:45:18 -0400

    add comment for manacher

--- src/string/manacher.hpp
+++ src/string/manacher.hpp
@@ -2,14 +2,19 @@
 #include <array>
 #include <vector>
 
-// return [even, odd]
-template <typename T>
-std::array<std::vector<int>, 2> manacher(const T& s) {
+// return [even, odd] where:
+//
+// even[i] is the half of the length of longest palindrome starting from the
+// i-th gap, the first gap is before the first character, there are n+1 gaps.
+//
+// odd[i] is half of the length of longest palindrome starting from the i-th
+// character.
+template <typename T> std::array<std::vector<int>, 2> manacher(const T &s) {
     int n = (int)size(s);
     std::array d{std::vector<int>(n + 1), std::vector<int>(n)};
 
     for (int z : {0, 1}) {
-        auto& p = d[z];
+        auto &p = d[z];
         for (int i = 0, l = 0, r = 0; i < n; i++) {
             int t = r - i + !z;
             if (i < r) {
