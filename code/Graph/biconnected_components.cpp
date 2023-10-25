0a530d5b213e4bb12e64b11a8ad4d89d907f29b5
Gengchen <tuogengchen@gmail.com>
2023-10-25 16:55:26 -0400

    update bcc and binary lifting

--- code/Graph/biconnected_components.cpp
+++ code/Graph/biconnected_components.cpp
@@ -1,46 +1,68 @@
 #include <vector>
 using namespace std;
-struct BCC {
-    int n, pos = 0;
-    vector<vector<int>> g;
-    vector<int> ord, low, cuts, stk;
-    vector<vector<int>> comps; // components
-    BCC(int n_) : n(n_), g(n), ord(n, -1), low(n) {}
-    void add_edge(int u, int v) {
-        g[u].push_back(v);
-        g[v].push_back(u);
-    }
-    void dfs(int u, int pa) {
-        low[u] = ord[u] = pos++;
-        stk.push_back(u);
-        int cnt=0;
-        bool is_cut = false;
-        for (auto v : g[u]) {
-            if (v == pa) continue;
-            if (ord[v] == -1) {
-                cnt++;
-                dfs(v, u);
-                low[u] = min(low[u], low[v]);
-                if (low[v] >= ord[u]) {
-                    if (u != pa || cnt > 1) is_cut = true;
-                    // the subtree will be disconnected if we remove vertex u,
-                    // do something if needed
-                    comps.emplace_back();
-                    while (true) {
-                        int back = stk.back();
-                        stk.pop_back();
-                        comps.back().push_back(back);
-                        if (back == v) break;
+int main() {
+    auto biconnected_comp = [&](const vector<vector<int>> &g) {
+        const int n = (int)size(g);
+        int pos = 0;
+        vector<int> ord(n, -1), low(n), cuts, stk;
+        vector<vector<int>> comps; // components
+
+        auto dfs = [&](auto &slf, int u, int pa) -> void {
+            low[u] = ord[u] = pos++;
+            stk.push_back(u);
+            int cnt = 0;
+            bool is_cut = false;
+            for (auto v : g[u]) {
+                if (v == pa)
+                    continue;
+                if (ord[v] == -1) {
+                    cnt++;
+                    slf(slf, v, u);
+                    low[u] = min(low[u], low[v]);
+                    if (low[v] >= ord[u]) {
+                        if (u != pa || cnt > 1)
+                            is_cut = true;
+                        // the subtree will be disconnected if we remove
+                        // vertex u, do something if needed
+                        comps.emplace_back();
+                        while (true) {
+                            int back = stk.back();
+                            stk.pop_back();
+                            comps.back().push_back(back);
+                            if (back == v)
+                                break;
+                        }
+                        comps.back().push_back(u);
                     }
-                    comps.back().push_back(u);
+                } else {
+                    low[u] = min(low[u], ord[v]);
                 }
-            } else low[u]=min(low[u], ord[v]);
-        }
-        if (is_cut) cuts.push_back(u);
-    }
-    void solve() {
+            }
+            if (is_cut)
+                cuts.push_back(u);
+        };
+
         for (int i = 0; i < n; i++) {
-            if (ord[i] == -1) dfs(i, i);
+            if (ord[i] == -1)
+                dfs(dfs, i, i);
         }
-    }
-};
+
+        return comps;
+    };
+}
+/*
+ * Extension: round-square tree
+ * Let c be the number of biconnected components in a graph G. The round-square
+ * tree consists of n round vertices and c square vertices. Each round vertex is
+ * connected to the square vertices corresponding to the biconnected components
+ * that the round vertex belongs to.
+ * The round-square tree is a tree with n + c vertices and n + c − 1 edges.
+ *
+ * Example (ABC 318G):
+ * Given a graph and three vertices A, B, C. Determine if there is a simple path
+ * connecting vertices A and C via vertex B.
+ *
+ * Solution:
+ * In the round-square tree, check if there is a square vertex on the path from
+ * A to C that is connected to B by an edge.
+ */
--- code/Graph/binary_lifting.cpp
+++ code/Graph/binary_lifting.cpp
@@ -1,41 +1,65 @@
-struct Binary_lifting {
-    const int sz, level;
-    const vector<vector<int>>& g;
-    vector<vector<int>> pa;
-    vector<int> dep;
-    Binary_lifting(const vector<vector<int>>& g_) :
-        sz((int)g_.size()),
-        level(__lg(sz)+2),
-        g(g_),
-        pa(sz, vector<int>(level)),
-        dep(g.size()) {}
-    void dfs(int u, int p) {
+#include <bits/stdc++.h>
+using namespace std;
+
+int __lg(int);
+int op(int, int);
+int e();
+int main() {
+    int n;
+    vector<vector<array<int, 2>>> g(n);
+
+    const int lg = __lg(n);
+    vector pa(n, vector(lg + 1, 0));
+    vector data(n, vector(lg + 1, 0)); // data[u][i]: data of path from u to pa[u][i]
+    data[0][0] = e();                  // set data[root][0] to identity element
+    vector<int> dep(n);
+
+    auto dfs = [&](auto& slf, int u, int p) -> void {
         pa[u][0] = p;
-        dep[u] = dep[p] + 1;
-        for (int i = 1; i < level; i++) {
+        for (int i = 1; i <= lg; i++) {
             pa[u][i] = pa[pa[u][i - 1]][i - 1];
+            data[u][i] = op(data[u][i - 1], data[pa[u][i - 1]][i - 1]);
         }
-        for (auto v : g[u]) {
+
+        for (auto [v, w] : g[u]) {
             if (v == p) continue;
-            dfs(v, u);
+            data[v][0] = w;
+            dep[v] = dep[u] + 1;
+            slf(slf, v, u);
         }
     };
-    int jump(int u, int step) {
-        for (int i=0; i<level; i++) {
-            if (step>>i&1) u=pa[u][i];
-        }
-        return u;
-    }
-    int lca(int x, int y) {
-        if (dep[x] > dep[y]) swap(x, y);
-        y=jump(y, dep[y] - dep[x]);
-        if (x == y) return x;
-        for (int i=level-1; i>=0; i--) {
-            if (pa[x][i] != pa[y][i]) {
-                x = pa[x][i];
-                y = pa[y][i];
+
+    dfs(dfs, 0, 0);
+
+    auto jump = [&](int u, int d) {
+        auto s = e();
+        for (int i = lg; i >= 0; i--) {
+            if (d >> i & 1) {
+                s = op(s, data[u][i]);
+                u = pa[u][i];
             }
         }
-        return pa[x][0];
-    }
-};
+        return pair{u, s};
+    };
+
+    auto lca = [&](int u, int v) {
+        if (dep[u] < dep[v]) {
+            swap(u, v);
+        }
+        int s = e();
+        tie(u, s) = jump(u, dep[u] - dep[v]);
+
+        if (u == v) return pair{u, s};
+
+        for (int i = lg; i >= 0; i--) {
+            if (pa[u][i] != pa[v][i]) {
+                s = op(op(s, data[u][i]), data[v][i]);
+                u = pa[u][i];
+                v = pa[v][i];
+            }
+        }
+
+        s = op(op(s, data[u][0]), data[v][0]);
+        return pair{pa[u][0], s};
+    };
+}
