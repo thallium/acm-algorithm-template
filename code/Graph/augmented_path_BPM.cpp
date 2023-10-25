631979242f4efd8e183844b0a2f5c108ccf46ed8
Gengchen <tuogengchen@gmail.com>
2023-10-25 15:09:53 -0400

    update aug path BPM

--- code/Graph/augmented_path_BPM.cpp
+++ code/Graph/augmented_path_BPM.cpp
@@ -1,64 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
 // augmented path algorithm for maximum-caredinality bipartite matching
 // Worst time complexity: O(nm), but very hard to hack (since we can shuffle),
 // usually runs extremely fast, 2e5 vertices and edges in 60 ms.
-mt19937 rng(1);
-struct aug_path {
-    vector<vector<int>> g;
-    vector<int> L, R, vis;
-    aug_path(int n, int m) : g(n), L(n, -1), R(m, -1), vis(n) {}
-    void add_edge(int a, int b) { g[a].push_back(b); }
-    bool match(int u) {
-        if (vis[u]) return false;
-        vis[u] = true;
-        for (auto v : g[u]) {
-            if (R[v] == -1) {
-                L[u] = v;
-                R[v] = u;
-                return true;
-            }
-        }
-        for (auto vec : g[u]) {
-            if (match(R[vec])) {
-                L[u] = vec;
-                R[vec] = u;
-                return true;
-            }
-        }
-        return false;
+
+int main() {
+    ios::sync_with_stdio(false);
+    int l, r, m;
+    cin >> l >> r >> m;
+    vector<vector<int>> g(l);
+    while (m--) {
+        int u, v;
+        cin >> u >> v;
+        u--, v--;
+        g[u].push_back(v); // note how we build the graph
     }
-    int solve() {
-        // shuffle to avoid counter test case, but may be slightly slower
-        // for (auto& v : g) 
+    auto aug_path = [](int n, int m, const vector<vector<int>> &g) {
+        // we can shuffle vertices:
+        // for (auto& v : g)
         //     shuffle(v.begin(), v.end(), rng);
+        vector<int> l(n, -1), r(m, -1), vis(n);
+        auto match = [&](auto& slf, int u) {
+            if (vis[u]) return false;
+            vis[u] = true;
+            for (auto v : g[u]) {
+                if (r[v] == -1) {
+                    l[u] = v;
+                    r[v] = u;
+                    return true;
+                }
+            }
+            for (auto v : g[u]) {
+                if (slf(slf, r[v])) {
+                    l[u] = v;
+                    r[v] = u;
+                    return true;
+                }
+            }
+            return false;
+        };
+
+        // We can also shuffle the order of visiting vertices
         // vector<int> order(L.size());
         // iota(order.begin(), order.end(), 0);
         // shuffle(order.begin(), order.end(), rng);
         bool ok = true;
         while (ok) {
-            ok=false;
+            ok = false;
             fill(vis.begin(), vis.end(), 0);
-            // for (auto i : order)
-            for (int i = 0; i < (int)L.size(); ++i)
-                if (L[i] == -1) ok |= match(i);
+            for (int i = 0; i < n; ++i) {
+                if (l[i] == -1) ok |= match(match, i);
+            }
         }
-        int ret = 0;
-        for (int i = 0; i < L.size(); ++i)
-            ret += (L[i] != -1);
-        return ret;
-    }
-};
-int main() {
-    ios::sync_with_stdio(false);
-    int l, r, m;
-    cin>>l>>r>>m;
-    aug_path g(l, r);
-    while (m--) {
-        int u, v;
-        cin>>u>>v;
-        g.add_edge(u, v);
-    }
-    cout<<g.solve()<<'\n';
-    for (int i=0; i<l; i++) {
-        if (g.L[i]!=-1) cout<<i<<' '<<g.L[i]<<'\n';
-    }
+
+        std::vector<std::pair<int, int>> matches;
+        for (size_t i = 0; i < n; i++) {
+            if (l[i] != -1) {
+                matches.emplace_back(i, l[i]);
+            }
+        }
+        return matches;
+    };
+    auto res = aug_path(l, r, g);
 }
