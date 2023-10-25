1ad0ab4691cf44f5f57b215e4c2a31728a91c09c
Gengchen <tuogengchen@gmail.com>
2023-10-24 21:58:58 -0400

    rewrite dsu on tree

--- code/Graph/dsu_on_tree.cpp
+++ code/Graph/dsu_on_tree.cpp
@@ -1,35 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
 int main() {
-    vector<int> bch(n, -1);
-    int cur_big = -1;
-    auto get_big = [&](auto &dfs, int u, int p) -> int {
-        int sz = 1, mx = 0;
+    int n;
+    vector<vector<int>> g(n);
+
+    vector<int> sz(n, 1), big(n, -1);
+    auto cal_size = [&](auto &slf, int u, int p) -> void {
         for (auto v : g[u]) {
-            if (v == p) continue;
-            int csz = dfs(dfs, v, u);
-            if (csz > mx) mx = csz, bch[u] = v;
-            sz += csz;
+            if (v == p)
+                continue;
+            slf(slf, v, u);
+            sz[u] += sz[v];
+            if (big[u] == -1 || sz[v] > sz[big[u]]) {
+                big[u] = v;
+            }
         }
-        return sz;
     };
-    auto add = [&](auto &slf, int u, int p, int x) -> void {
-        // update info of u here
+    cal_size(cal_size, 0, 0);
+    auto modify = [&](auto &slf, int u, int p, int add) -> void {
+        if (add) {
+            // add u to result
+        } else {
+            // remove u from result
+        }
         for (auto v : g[u]) {
-            if (v == p || v == cur_big) continue;
-            slf(slf, v, u, x);
+            if (v == p)
+                continue;
+            slf(slf, v, u, add);
         }
     };
-    auto dfs = [&](auto &dfs, int u, int pa, bool keep) -> void {
-        int big = bch[u];
-        for (auto v : g[u])
-            if (v != pa && v != big) dfs(dfs, v, u, 0);
-        if (big != -1) {
-            dfs(dfs, big, u, 1);
-            cur_big = big;
+    auto dfs = [&](auto &slf, int u, int p) -> void {
+        for (auto v : g[u]) {
+            if (v == p || v == big[u])
+                continue;
+            slf(slf, v, u);          // tranverse light child
+            modify(modify, v, u, 0); // remove light child
         }
-        add(add, u, pa, 1);
-        // now you get all the info of subtree of u, answer queries about u
-        // here.
-        cur_big = -1;
-        if (!keep) add(add, u, pa, -1);
+        if (big[u] != -1) {
+            slf(slf, big[u], u); // remove heavy child
+        }
+        for (auto v : g[u]) {
+            if (v == p || v == big[u])
+                continue;
+            modify(modify, v, u, 1); // add light child again
+        }
+        // add u to result
+        // now we have the result for subtree of u
     };
+    dfs(dfs, 0, 0);
 }
