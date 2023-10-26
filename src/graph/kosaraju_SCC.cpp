ff15b82b7fccd0be1f638f17342feeb6d9ac63a6
Gengchen <tuogengchen@gmail.com>
2023-10-26 11:07:08 -0400

    refactor kosaraju

--- src/graph/kosaraju_SCC.cpp
+++ src/graph/kosaraju_SCC.cpp
@@ -1,38 +1,36 @@
-struct kosaraju {
-    int n;
-    vector<bool> vis;
-    vector<int> color, order;
-    vector<vector<int>> g, g2, comp;
-    kosaraju(int n_) : n(n_), vis(n), color(n, -1), g(n), g2(n) {}
-    void add_edge(int u, int v) {
-        g[u].push_back(v);
-        g2[v].push_back(u);
-    }
-    void dfs1(int u) {
-        vis[u] = true;
-        for (int v : g[u])
-            if (!vis[v]) dfs1(v);
-        order.push_back(u);
-    }
-    void dfs2(int u) {
-        comp.back().push_back(u);
-        for (int v : g2[u])
-            if (color[v] == -1)
-                dfs2(v);
-    }
-    void solve() {
-        for (int i = 0; i < n; ++i)
-            if (!vis[i]) dfs1(i);
-        for (int i = n - 1; i >= 0; --i)
-            if (color[order[i]] == -1) {
-                comp.emplace_back();
-                dfs2(order[i]);
-            }
-        // reverse(comp.begin(), comp.end()); to sort components in topological
-        // order
-        for (int i = 0; i < (int)comp.size(); i++) {
-            for (int x : comp[i])
-                color[x] = i;
+#include <vector>
+using namespace std;
+inline auto scc(const std::vector<std::vector<int>>& g) -> std::pair<int, std::vector<int>> {
+    int n = (int)size(g);
+    vector<vector<int>> g2(n);
+    for (int i = 0; i < n; i++) {
+        for (auto v : g[i]) {
+            g2[v].push_back(i);
         }
     }
-};
+    vector<int> vis(n), color(n), ord;
+    auto dfs = [&](auto& slf, int u) -> void {
+        vis[u] = 1;
+        for (int v : g[u])
+            if (!vis[v]) slf(slf, v);
+        ord.push_back(u);
+    };
+    for (int i = 0; i < n; i++) {
+        if (!vis[i]) dfs(dfs, i);
+    }
+
+    int c = 0;
+    auto dfs2 = [&](auto& slf, int u) -> void {
+        color[u] = c;
+        for (int v : g2[u])
+            if (color[v] == -1)
+                slf(slf, v);
+    };
+    for (int i = n - 1; i >= 0; --i) {
+        if (color[ord[i]] == -1) {
+            dfs2(dfs2, ord[i]);
+            c++;
+        }
+    }
+    return {c, color};
+}
